34a492ecdf11fb937316269d85e5db1967920133
YordanM-IL <yordan@infinitelambda.com>
2024-02-06 19:37:58 +0200

    Implemented method factory pattern

--- factory/src/main/java/factoryMethod/VehicleDealer.java
+++ factory/src/main/java/factoryMethod/VehicleDealer.java
@@ -6,14 +6,14 @@ public class VehicleDealer {
 
     public static void main(String[] args) {
 
-//        final List<VehicleFactory> vehicleFactories = List.of(
-//                new FamilyCarFactory(),
-//                new SportCarFactory(),
-//                new TraderCarFactory()
-//        );
-//
-//        vehicleFactories.stream()
-//                .map(VehicleFactory::getVehicle)
-//                .forEach(Vehicle::drive);
+        final List<VehicleFactory> vehicleFactories = List.of(
+                new FamilyCarFactory(),
+                new SportCarFactory(),
+                new TraderCarFactory()
+        );
+
+        vehicleFactories.stream()
+                .map(VehicleFactory::getVehicle)
+                .forEach(Vehicle::drive);
     }
 }
