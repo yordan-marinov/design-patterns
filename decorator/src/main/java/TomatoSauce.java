d5b01e4d6c3006c756b18f91d1fbe37fa86dc5ca
YordanM-IL <yordan@infinitelambda.com>
2024-02-06 11:54:17 +0200

    Format code by reducing the duplication inside the concrete Toppings classes and moved the logic in to the abstract Decorator class instead

--- decorator/src/main/java/TomatoSauce.java
+++ decorator/src/main/java/TomatoSauce.java
@@ -1,19 +1,11 @@
 public class TomatoSauce extends ToppingDecorator {
 
-    private static final double COST = 1.00;
-    private final String toppingName;
     public TomatoSauce(Pizza basePizza) {
         super(basePizza);
-        this.toppingName = this.getClass().getSimpleName().toLowerCase();
     }
 
     @Override
-    public String getDescription() {
-        return String.format("%s, %s", basePizza.getDescription(), toppingName) ;
-    }
-
-    @Override
-    public double cost() {
-        return basePizza.cost() + COST;
+    public double getCost() {
+        return 1.00;
     }
 }
--- decorator/src/main/java/ToppingDecorator.java
+++ decorator/src/main/java/ToppingDecorator.java
@@ -4,18 +4,23 @@ import lombok.Data;
 abstract public class ToppingDecorator implements Pizza{
 
     protected Pizza basePizza;
+    private final String toppingName;
+    private final double cost;
+
 
     public ToppingDecorator(Pizza basePizza) {
         this.basePizza = basePizza;
+        this.toppingName = this.getClass().getSimpleName().toLowerCase();
+        this.cost = this.getCost();
     }
 
     @Override
     public String getDescription() {
-        return basePizza.getDescription();
+        return String.format("%s, %s", basePizza.getDescription(), toppingName);
     }
 
     @Override
     public double cost() {
-        return basePizza.cost();
+        return basePizza.cost() + cost;
     }
 }
